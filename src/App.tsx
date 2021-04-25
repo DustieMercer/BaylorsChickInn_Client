95893d83c161d7bbd8017847f0a120bdd0da5b38
Dustie Mercer <d.nichole.mercer@gmail.com>
2021-04-25 08:21:47 -0400

    logout complete

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import NavBar from "./components/public/NavBar";
 import Footer from "./components/public/footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
 
+
+
+
 class App extends Component<any,any> {
   constructor(props:any) {
     super(props); 
